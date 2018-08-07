d75f9de664d3d190e97594d0759d709e776d9b36
paidforby <ggallo102@gmail.com>
2018-08-07 10:13:50 -0700

    saving script this time

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # youtube-pl
-tired of rewriting this
+tired of rewriting this shell script that utilizes youtube-dl to organize your locally cached music directories.
+
---
+++ pl-dl.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+read -p "Artist: " ARTIST
+read -p "Album: " ALBUM
+read -p "Link: " LINK
+
+mkdir "$ARTIST" 
+mkdir "$ARTIST/$ALBUM" 
+cd "$ARTIST/$ALBUM"
+
+youtube-dl -x --audio-format mp3 -o "%(index)s-%(title)s.%(ext)s" $LINK
